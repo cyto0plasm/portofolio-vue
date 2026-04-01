fa7a7f5cc69c9b585f6911c582a1084b5f227923
zaki <yousif>
2026-04-01 02:14:18 +0200

    security fix

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import {getAuth } from  'firebase/auth'
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCptWXOHXiPEgDjxGxgYSTm2VDtddDiBWU",
-  authDomain: "portofolio-c91e6.firebaseapp.com",
-  projectId: "portofolio-c91e6",
-  storageBucket: "portofolio-c91e6.firebasestorage.app",
-  messagingSenderId: "160085420112",
-  appId: "1:160085420112:web:b1613f790ff9598d384955",
-  measurementId: "G-NF228K134M"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
